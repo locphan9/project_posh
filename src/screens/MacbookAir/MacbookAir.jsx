3a2fb3c39d6083f9f4e4ed818f5528c964b60855
locphan9 <58446220+locphan9@users.noreply.github.com>
2023-07-16 18:42:32 -0400

    Initial commit

---
+++ src/screens/MacbookAir/MacbookAir.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookAir = () => {
+  return (
+    <div className="macbook-air">
+      <div className="div">
+        <div className="overlap">
+          <div className="text-wrapper">(päSH)</div>
+          <div className="text-wrapper-2">Posh your project</div>
+          <div className="frame">
+            <div className="rectangle" />
+          </div>
+          <h1 className="h-1">Posh</h1>
+          <div className="ellipse" />
+          <div className="ellipse-2" />
+          <img
+            className="img"
+            alt="Rectangle"
+            src="https://generation-sessions.s3.amazonaws.com/936a21c79d8279582ee683b49aadd70b/img/rectangle-2.png"
+          />
+          <div className="div-wrapper">
+            <div className="text-wrapper-3">Create a posh</div>
+          </div>
+          <p className="p">Move faster with 8,294 Engineering students at Penn State</p>
+          <img
+            className="dont-get-stuck-on"
+            alt="Dont get stuck on"
+            src="https://generation-sessions.s3.amazonaws.com/936a21c79d8279582ee683b49aadd70b/img/don-t-get-stuck-on-your-coding-projects.png"
+          />
+        </div>
+        <div className="overlap-group">
+          <div className="frame-2">
+            <div className="frame-3">
+              <div className="text-wrapper-4">P</div>
+            </div>
+            <div className="text-wrapper-5">|Posh</div>
+            <div className="text-wrapper-6">| Ask</div>
+            <div className="text-wrapper-6">|Show</div>
+          </div>
+          <div className="frame-4">
+            <div className="text-wrapper-4">P</div>
+          </div>
+        </div>
+        <div className="frame-5">
+          <div className="frame-6">
+            <div className="text-wrapper-7">Back End</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper-7">Analytics</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper-7">Computer Vision</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper-7">Natural Language Processing</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper-7">ChatGPT</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper-7">Homework</div>
+          </div>
+          <div className="frame-6">
+            <div className="text-wrapper-8">Analytics</div>
+          </div>
+        </div>
+        <div className="frame-7">
+          <img
+            className="page-info"
+            alt="Page info"
+            src="https://generation-sessions.s3.amazonaws.com/936a21c79d8279582ee683b49aadd70b/img/page-info.svg"
+          />
+          <div className="text-wrapper-9">Filters</div>
+        </div>
+        <div className="frame-8">
+          <div className="text-wrapper-10">New Poshes</div>
+          <div className="frame-9">
+            <div className="rectangle-2" />
+            <div className="rectangle-2" />
+            <div className="rectangle-2" />
+          </div>
+        </div>
+        <img
+          className="frame-10"
+          alt="Frame"
+          src="https://generation-sessions.s3.amazonaws.com/936a21c79d8279582ee683b49aadd70b/img/frame-23.svg"
+        />
+        <div className="text-wrapper-11">New Poshes</div>
+        <div className="frame-11">
+          <div className="frame-12">
+            <div className="rectangle-3" />
+            <div className="frame-13">
+              <div className="text-wrapper-12">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-9">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-9">Pay: No Pay</div>
+          </div>
+          <div className="frame-12">
+            <div className="rectangle-3" />
+            <div className="frame-13">
+              <div className="text-wrapper-12">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-9">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-9">Pay: No Pay</div>
+          </div>
+          <div className="frame-12">
+            <div className="rectangle-3" />
+            <div className="frame-13">
+              <div className="text-wrapper-12">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-9">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-9">Pay: No Pay</div>
+          </div>
+          <div className="frame-12">
+            <div className="rectangle-3" />
+            <div className="frame-13">
+              <div className="text-wrapper-12">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-9">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-9">Pay: No Pay</div>
+          </div>
+          <div className="frame-12">
+            <div className="rectangle-3" />
+            <div className="frame-13">
+              <div className="text-wrapper-12">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-9">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-9">Pay: No Pay</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
