f41ed3d7c92b992887549a7064f9bb1cdba37557
locphan9 <phanduongbaoloc9999@gmail.com>
2023-07-17 00:12:49 -0400

    Please commit

---
+++ src/screens/PoshInterface/PoshInterface.jsx
@@ -0,0 +1,191 @@
+import React from "react";
+import "./style.css";
+
+export const PoshInterface = () => {
+  return (
+    <div className="posh-interface">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="top-banner">
+            <h1 className="text-wrapper">Posh</h1>
+            <div className="text-wrapper-2">(päSH)</div>
+            <div className="text-wrapper-3">Posh your project</div>
+          </div>
+          <div className="ellipse" />
+        </div>
+        <div className="overlap">
+          <div className="banner">
+            <img className="rectangle" alt="Rectangle" src="/img/rectangle-2.png" />
+            <p className="p">Move faster with 8,294 Engineering students at Penn State</p>
+            <img
+              className="dont-get-stuck-on"
+              alt="Dont get stuck on"
+              src="/img/don-t-get-stuck-on-your-coding-projects.png"
+            />
+          </div>
+          <img className="create-posh-button" alt="Create posh button" src="/img/create-posh-button.png" />
+        </div>
+        <div className="project-feature">
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">Back End</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">Analytics</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">Computer Vision</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">Natural Language Processing</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">ChatGPT</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-4">Homework</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-5">Analytics</div>
+          </div>
+        </div>
+        <div className="filter-button">
+          <img className="page-info" alt="Page info" src="/img/page-info.svg" />
+          <div className="text-wrapper-6">Filters</div>
+        </div>
+        <div className="frame">
+          <div className="text-wrapper-7">P</div>
+          <div className="text-wrapper-7">P</div>
+          <div className="text-wrapper-8">|Posh</div>
+          <div className="text-wrapper-9">| Ask</div>
+          <div className="text-wrapper-9">|Show</div>
+        </div>
+        <div className="text-wrapper-10">New Poshes</div>
+        <div className="new-posh-frame">
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+        </div>
+        <div className="text-wrapper-12">Community Upvote</div>
+        <div className="new-posh-frame-2">
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+          <div className="frame-2">
+            <div className="rectangle-2" />
+            <div className="frame-3">
+              <div className="text-wrapper-11">Hiring</div>
+            </div>
+            <p className="building-backend-web">
+              Building backend web application <br />
+              with Flask
+            </p>
+            <p className="text-wrapper-6">Position: Software Engineer, Data Scientist</p>
+            <div className="text-wrapper-6">Pay: No Pay</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/PoshInterface/index.js
@@ -0,0 +1,1 @@
+export { PoshInterface } from "./PoshInterface";
